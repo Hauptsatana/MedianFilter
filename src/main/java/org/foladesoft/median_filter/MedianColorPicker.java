4ac9e4f17e212abab00dc581442976a6160dfd26
Hauptsatana <hauptsatana@gmail.com>
2016-12-24 06:47:42 +0500

    Deleted unused imports

--- src/main/java/org/foladesoft/median_filter/MedianColorPicker.java
+++ src/main/java/org/foladesoft/median_filter/MedianColorPicker.java
@@ -9,9 +9,7 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 /**
  *
